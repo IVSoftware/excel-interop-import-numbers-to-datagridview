b7aaa1cf8dcd14512321b760cf93f016c00818a6
Tom Gregor <tgregor@ivsoftware.com>
2023-02-03 18:09:56 -0700

    Add project files.

---
+++ excel-interop-import-numbers-to-datagridview/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace excel_interop_import_numbers_to_datagridview
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonImport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.BackgroundColor = System.Drawing.Color.Azure;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView.Location = new System.Drawing.Point(189, 8);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 33;
+            this.dataGridView.Size = new System.Drawing.Size(936, 824);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonImport.Location = new System.Drawing.Point(12, 12);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(150, 45);
+            this.buttonImport.TabIndex = 1;
+            this.buttonImport.Text = "Import";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 844);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button buttonImport;
+    }
+}
+
---
+++ excel-interop-import-numbers-to-datagridview/MainForm.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using Range = Microsoft.Office.Interop.Excel.Range;
+using System.Diagnostics;
+
+namespace excel_interop_import_numbers_to_datagridview
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            // Create
+            _xlApp = new Microsoft.Office.Interop.Excel.Application();
+            // When in the future the main form closes, dispose the Excel interop.
+            Disposed += (sender, e) =>
+            {
+                _xlBook?.Close();
+                _xlApp.Quit();
+            }; 
+            buttonImport.Click += Import_Click_1;
+        }
+        private readonly Microsoft.Office.Interop.Excel.Application _xlApp;
+        private Workbook _xlBook = null;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            dataGridView.DataSource = Records;
+
+            #region F O R M A T    C O L U M N S
+            Records.Add(new Record());
+            foreach (DataGridViewColumn column in dataGridView.Columns)
+            {
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                if (column.Index > 1) column.DefaultCellStyle.Format = "F2";
+            }
+            Records.Clear();
+            #endregion F O R M A T    C O L U M N S          
+        }
+        BindingList<Record> Records { get; } = new BindingList<Record>();
+
+        private void Import_Click_1(object sender, EventArgs e)
+        {
+            Records.Clear();
+            string filePath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Excel",
+                "testdata.xlsx");
+
+            _xlBook = _xlApp.Workbooks.Open(filePath);
+            Worksheet xlSheet = _xlBook.Sheets[1];
+            Range xlRange = xlSheet.UsedRange;
+
+            Range range;
+            List<string> 
+                headers = new List<string>(),
+                line = new List<string>();
+
+            for (int i = 1; i <= xlRange.Rows.Count; i++)
+            {
+                if (i.Equals(1))
+                {
+                    for (int j = 1; j <= xlRange.Columns.Count; j++)
+                    {
+                        range = xlRange.Cells[i, j];
+                        headers.Add(range.Value2);
+                    }
+                }
+                else
+                {
+                    var record = new Record();
+                    for (int j = 1; j <= xlRange.Columns.Count; j++)
+                    {
+                        range = xlRange.Cells[i, j];
+                        var name = headers[j - 1];
+                        switch(name)
+                        {
+                            case "Datum": record.Datum = DateTime.FromOADate(range.Value2); break;
+                            case "Energia": record.Energia = range.Value2; break;
+                            case "AC výkon": record.ACvýkon = range.Value2; break;
+                            case "napetie siete": record.napetiesiete = range.Value2; break;
+                            case "AC prud": record.ACprud = range.Value2; break;
+                            case "DC napetie": record.DCnapetie = range.Value2; break;
+                            default:
+                                Debug.Assert(false, $"Not recognized: '{name}'");
+                                break;
+                        }
+                    }
+                    Records.Add(record);
+                }
+            }
+        }
+    }
+
+    class Record
+    {
+        [DisplayName("Datum")]
+        public DateTime Datum { get; set; }
+
+        [DisplayName("Energia")]
+        public double Energia { get; set; }
+
+        [DisplayName("AC výkon")]
+        public double ACvýkon { get; set; }
+
+        [DisplayName("napetie siete")]
+        public double napetiesiete { get; set; }
+
+        [DisplayName("AC prud")]
+        public double ACprud { get; set; }
+
+        [DisplayName("DC napetie")]
+        public double DCnapetie { get; set; }
+    }
+}
